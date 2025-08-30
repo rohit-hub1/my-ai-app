ff6b3538f0a8aaa25f28f0310f93c8dc90181126
rohit-hub1 <rohitsolanki8383@gmail.com>
2025-08-30 11:53:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+// app/page.tsx
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind Working 🚀</h1>
-    </div>
-  );
+  redirect("/login");
 }
